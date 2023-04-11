221583de7ca004444202a10876ae1bb88b497947
Christopher Melgar <cmelgarmorales@csu.fullerton.edu>
2023-04-11 11:42:19 -0700

    API doesnt close properly, fixed emu and api

--- backend/flask-python/app.py
+++ backend/flask-python/app.py
@@ -129,10 +129,13 @@ def configHelper(channel,PORT,mix,HOST,isDummy):
             jsonFormat["mixes"].append({str(i): mix_dict})
         if isDummy:
             # Send signal to emulator to kill itself
-            s.sendall(TERMINATE.encode())
+            s.send(TERMINATE.encode())
+            s.shutdown(socket.SHUT_RDWR)
+            s.close()
             # wait for thread to finish dying
             thread.join()
         else:
+            s.shutdown(socket.SHUT_RDWR)
             s.close()
     # append datatime to json
     now = datetime.datetime.now()
