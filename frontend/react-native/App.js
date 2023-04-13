2b2f6c0b26133192bde7278c5934629c81cc749b
Brian Miller <bcmiller@csu.fullerton.edu>
2023-04-13 21:34:23 +0000

    App.js edited to call TableComponent

--- frontend/react-native/App.js
+++ frontend/react-native/App.js
@@ -1,12 +1,15 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 import TestFieldAndButton from './TestFieldAndButton';
+import TableComponent from './TableComponent';
+
 export default function App() {
   return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
       <Text>Edits to App</Text>
-      <TestFieldAndButton></TestFieldAndButton>
+      <TestFieldAndButton />
+      <TableComponent />
       <StatusBar style="auto" />
       <Text>More edits</Text>
     </View>
